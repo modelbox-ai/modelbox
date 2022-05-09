ef8e1d5fee54e55e4a92e44b5dbc81379eadf1ff
Nick Peng <pymumu@gmail.com>
2022-05-09 20:02:42 +0800

    mnist: add test image for mnist

--- src/demo/mnist/graph/test_mnist.py
+++ src/demo/mnist/graph/test_mnist.py
@@ -17,16 +17,48 @@
 import base64
 import http.client
 import json
+import sys
+import os
+import random
+import argparse
 from pathlib import Path
+from PIL import Image
+from urllib.parse import urlparse
+import urllib.request
 
 source_path = Path(__file__).resolve()
 source_dir = source_path.parent
+mnist_image_path = str(source_dir) + "/mnist-image"
+
+def DisplayImage(image_path):
+    # pass the image as command line argument
+    img = Image.open(image_path)
+
+    # resize the image
+    width, height = img.size
+    aspect_ratio = height/width
+    new_width = 32
+    new_height = aspect_ratio * new_width * 0.50
+    img = img.resize((new_width, int(new_height)))
+
+    # convert image to greyscale format
+    img = img.convert('L')
+
+    pixels = img.getdata()
+
+    # replace each pixel with a character from array
+    chars = ["B","S","#","&","@","$","%","*","!",":"," "]
+    new_pixels = [chars[pixel//25] for pixel in pixels]
+    new_pixels = ''.join(new_pixels)
+
+    # split string of chars into multiple strings of length equal to new width and create a list
+    new_pixels_count = len(new_pixels)
+    ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+    ascii_image = "\n".join(ascii_image)
+    sys.stdout.write(ascii_image + "\n")
 
 class HttpConfig:
     def __init__(self, img_base64_str):
-        self.hostIP = "127.0.0.1"
-        self.Port = 8190
-
         self.httpMethod = "POST"
         self.requstURL = "/v1/mnist_test"
 
@@ -40,17 +72,79 @@ class HttpConfig:
 
         self.body = json.dumps(self.test_data)
 
-img_path = str(source_dir) + "/mnist_0.png"
+def DoMnistInfer(host, img_path, PrintRequest =  False):
+    o = urlparse('//' + host)
 
-with open(img_path, 'rb') as fp:
-    base64_data = base64.b64encode(fp.read())
-    img_base64_str = str(base64_data, encoding='utf8')
+    with open(img_path, 'rb') as fp:
+        base64_data = base64.b64encode(fp.read())
+        img_base64_str = str(base64_data, encoding='utf8')
 
-http_config = HttpConfig(img_base64_str)
+    http_config = HttpConfig(img_base64_str)
 
-conn = http.client.HTTPConnection(host=http_config.hostIP, port=http_config.Port)
-conn.request(method=http_config.httpMethod, url=http_config.requstURL, body=http_config.body,
-             headers=http_config.headerdata)
+    http_config.hostIP = o.hostname
+    http_config.Port = o.port
 
-response = conn.getresponse().read().decode()
-print(response)
+    if PrintRequest:
+        print("-- Request Body:")
+        print(http_config.body)
+
+    conn = http.client.HTTPConnection(host=http_config.hostIP, port=http_config.Port)
+    try:
+        conn.request(method=http_config.httpMethod, url=http_config.requstURL, body=http_config.body,
+                headers=http_config.headerdata)
+    except  Exception as e:
+        print("Connect to " + host + " failed, reaseon: ")
+        print(e)
+        print("Please check the service status, or use '-host [ip:port]' to specify the address of the service.")
+        return 1
+
+    print("-- Response:")
+    response = conn.getresponse().read().decode()
+    print(response)
+    return 0
+
+def Extract(download):
+    if os.path.exists(mnist_image_path):
+        return
+    
+    if download:
+        urllib.request.urlretrieve("http://download.modelbox-ai.com/test/mnist-image.tar.gz", "mnist-image.tar.gz")
+
+    os.system("tar -C "+ str(source_dir) + " -xf mnist-image.tar.gz")
+
+def main():
+    descStr = "This program is used to test mnist inference."
+    parser = argparse.ArgumentParser(description=descStr)
+    parser.add_argument('-id', dest='TestNum', help="Test image id.", required=False)
+    parser.add_argument('-download', dest='DownLoad', help="download full test image from modelbox.", required=False, default=False, action='store_true')
+    parser.add_argument('-print-request', dest='PrintRequest', help="print request json body.", required=False, default=False, action='store_true')
+    parser.add_argument('-host', dest='Host', default="127.0.0.1:8190", help="set host and port.", required=False)
+
+    args = parser.parse_args()
+    TestNum = 0
+
+    Extract(args.DownLoad)
+
+    if args.TestNum:
+        TestNum = args.TestNum
+    else:
+        list = os.listdir(mnist_image_path)
+        TestNum = random.randint(0, len(list) - 1)
+
+    img_path = mnist_image_path + "/test_" + str(TestNum) + ".bmp"
+
+    if not os.path.exists(img_path):
+        print("image " + img_path + " not exists")
+        return 1
+
+    Host = args.Host
+    PrintRequest = args.PrintRequest
+
+    print("-- Image ID: " + str(TestNum) + ", Image: " )
+    DisplayImage(img_path)
+    print("-- Connect to " + Host)
+    return DoMnistInfer(Host, img_path, PrintRequest)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
