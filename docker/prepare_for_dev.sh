f3dfa4f1d27102f4c4b3309658f6454ae9cf47e0
zxk114 <xianku.zhang@gmail.com>
2022-07-28 15:43:16 +0800

    add tensorrt 8.4.2

--- docker/prepare_for_dev.sh
+++ docker/prepare_for_dev.sh
@@ -5,17 +5,28 @@ echo "VERSION_ID:$VERSION_ID"
 download() {
     url="$1"
     softName=${url##*/}
-    echo -e "\n\nBegen to download ${softName}"
-    curl -LO ${url}
+    echo -e "\n\nBegin to download ${softName}"
+    curl -k -L -O ${url}
 
-    if [ "$(ls -lh ${softName}|awk '{print $5}')" == "0" ]; then
-        echo "package download failed"
+    times=1
+    until [ $times -gt 3 ]; do
+        if [ $(ls -l ${softName}|awk '{print $5}') -lt 50000 ]; then
+            ls -lh ${softName}
+            echo "package ${softName} download failed, retry $times ......"
+            curl -k -L -O ${url}
+        fi
+        let "times++"
+    done
+
+    if [ $(ls -l ${softName}|awk '{print $5}') -lt 50000 ]; then
+        echo "package ${softName} download failed,pls check"
         exit 1
     fi
 }
 
 if [ "$(arch)" == "x86_64" ];then
     if [ "$VERSION_ID" == "18.04" ];then
+        download https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/opencv_4.2.0_dev-ubuntu.tar.gz
         download https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/obssdk_3.22.3_dev-ubuntu.tar.gz
     elif [ "$VERSION_ID" == "20.04" ];then
         download https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/cpprestsdk_2.10.15_dev.tar.gz
@@ -28,6 +39,7 @@ if [ "$(arch)" == "x86_64" ];then
     download https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/Video_Codec_SDK_9.1.23.tar.gz
 elif [ "$(arch)" == "aarch64" ];then
     if [ "$VERSION_ID" == "18.04" ];then
+        download http://download.modelbox-ai.com/third-party/aarch64/opencv_4.2.0_dev-ubuntu.tar.gz
         download http://download.modelbox-ai.com/third-party/aarch64/obssdk_3.22.3_dev-ubuntu.tar.gz
     elif [ "$VERSION_ID" == "20.03" ];then
         download http://download.modelbox-ai.com/third-party/aarch64/cpprestsdk_2.10.18_dev.tar.gz
@@ -37,9 +49,6 @@ elif [ "$(arch)" == "aarch64" ];then
         download http://download.modelbox-ai.com/third-party/aarch64/obssdk_3.22.3_dev.tar.gz
     fi
     download http://download.modelbox-ai.com/third-party/sdk/nlohmann-json_3.7.3.tar.gz
-else
-    echo "build error"
-    exit 1
 fi
 
 ls -lh *.tar.gz
