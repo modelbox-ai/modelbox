03a62fedcb66db02255211d0777bfaa77b7690b8
zxk114 <xianku.zhang@gmail.com>
2022-08-18 09:58:07 +0800

    build: armd image build optimization & fix some issue

--- docker/prepare_for_run.sh
+++ docker/prepare_for_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CUR_DIR=$(cd $(dirname "${BASH_SOURCE[0]}");pwd)
 VERSION_ID=$(sed -nr '/VERSION_ID/s/^VERSION_ID="(.*)"$/\1/gp' /etc/os-release)
 echo "VERSION_ID:$VERSION_ID"
 
@@ -36,11 +37,19 @@ if [ "$(arch)" == "x86_64" ];then
         download https://github.com/modelbox-ai/modelbox-binary/releases/download/BinaryArchive/obssdk_3.22.3.tar.gz
     fi
 elif [ "$(arch)" == "aarch64" ];then
-    if [ "$VERSION_ID" == "22.04" ];then
+    if [ "$VERSION_ID" == "18.04" ];then
+        cp -af /opt/ubuntu/* .
+        sed -i '1d;5d' ${CUR_DIR}/Dockerfile.ascend.runtime.ubuntu
+        sed -i '/COPY release/a\COPY Ascend_run /usr/local/Ascend' ${CUR_DIR}/Dockerfile.ascend.runtime.ubuntu
+        sed -i '/COPY release/a\COPY npu-smi /usr/local/sbin/npu-smi' ${CUR_DIR}/Dockerfile.ascend.runtime.ubuntu
         download http://download.modelbox-ai.com/third-party/aarch64/opencv_4.2.0-ubuntu.tar.gz
         download http://download.modelbox-ai.com/third-party/aarch64/obssdk_3.22.3-ubuntu.tar.gz
     elif [ "$VERSION_ID" == "20.03" ];then
-        download http://download.modelbox-ai.com/third-party/aarch64/cpprestsdk_2.10.15.tar.gz
+        cp -af /opt/openeuler/* .
+        sed -i '1d;5d' ${CUR_DIR}/Dockerfile.ascend.runtime.openeuler
+        sed -i '/COPY release/a\COPY Ascend_run /usr/local/Ascend' ${CUR_DIR}/Dockerfile.ascend.runtime.openeuler
+        sed -i '/COPY release/a\COPY npu-smi /usr/local/sbin/npu-smi' ${CUR_DIR}/Dockerfile.ascend.runtime.openeuler
+        download http://download.modelbox-ai.com/third-party/aarch64/cpprestsdk_2.10.18.tar.gz
         download http://download.modelbox-ai.com/third-party/aarch64/duktape_2.6.0.tar.gz
         download http://download.modelbox-ai.com/third-party/aarch64/ffmpeg_4.4.tar.gz
         download http://download.modelbox-ai.com/third-party/aarch64/opencv_4.2.0.tar.gz
